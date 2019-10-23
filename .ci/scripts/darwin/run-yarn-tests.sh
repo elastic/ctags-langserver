83415df645f23f89d03ae74f1d9f5566afd7cf04
Manuel de la Peña <mdelapenya@gmail.com>
2019-10-23 17:30:17 +0200

    Do not install packages if already present
    This problem comes when the build agents are not immutable

--- .ci/scripts/darwin/run-yarn-tests.sh
+++ .ci/scripts/darwin/run-yarn-tests.sh
@@ -3,20 +3,43 @@ set -euxo pipefail
 
 NODE_VERSION=${1:?"Node version is required"}
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
+function install_nvm() {
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
 
-source "${NVM_DIR}/nvm.sh"
+    source "${NVM_DIR}/nvm.sh"
 
-nvm --version
+    nvm --version
+}
 
-nvm install ${NODE_VERSION}
+function install_node() {
+    nvm install ${NODE_VERSION}
 
-nvm use --delete-prefix ${NODE_VERSION}
+    nvm use --delete-prefix ${NODE_VERSION}
+}
 
-touch ~/.bashrc
+function install_yarn() {
+    touch ~/.bashrc
 
-curl -o- -L https://yarnpkg.com/install.sh | bash
+    curl -o- -L https://yarnpkg.com/install.sh | bash
+}
 
-yarn install
+function run_tests() {
+    yarn install
+    yarn test:ci
+}
 
-yarn test:ci
+function main() {
+    if ! [ -f "${HOME}/.nvm/nvm.sh" ]; then
+        install_nvm
+    fi
+
+    if ! [ -x "$(command -v node)" ]; then
+        install_node
+    fi
+
+    if ! [ -x "$(command -v yarn)" ]; then
+        install_yarn
+    fi
+}
+
+main "$@"
