49597bbc51fc3722a960cbfe69644998d3d07d4e
Manuel de la Peña <manuel.delapena@elastic.co>
2019-10-24 08:24:47 +0200

    Do not install packages if already present (#36)
    * Add Jenkins badge
    
    * Do not install packages if already present
    
    This problem comes when the build agents are not immutable
    
    * Run tests in the specific node version
    
    * Move environment variable setting to the pipeline
    
    This is intended to be used by CI, not developers, so let's set up everything in the pipeline

--- .ci/scripts/darwin/run-yarn-tests.sh
+++ .ci/scripts/darwin/run-yarn-tests.sh
@@ -3,20 +3,45 @@ set -euxo pipefail
 
 NODE_VERSION=${1:?"Node version is required"}
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
+function install_nvm() {
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
 
-source "${NVM_DIR}/nvm.sh"
+    source "${NVM_DIR}/nvm.sh"
 
-nvm --version
+    nvm --version
+}
 
-nvm install ${NODE_VERSION}
+function install_node() {
+    nvm install ${NODE_VERSION}
+}
 
-nvm use --delete-prefix ${NODE_VERSION}
+function install_yarn() {
+    touch ~/.bashrc
 
-touch ~/.bashrc
+    curl -o- -L https://yarnpkg.com/install.sh | bash
+}
 
-curl -o- -L https://yarnpkg.com/install.sh | bash
+function run_tests() {
+    yarn install
+    yarn test:ci
+}
 
-yarn install
+function main() {
+    if ! [ -f "${HOME}/.nvm/nvm.sh" ]; then
+        install_nvm
+    fi
 
-yarn test:ci
+    if ! [ -x "$(command -v node)" ]; then
+        install_node
+    fi
+
+    nvm use --delete-prefix ${NODE_VERSION}
+
+    if ! [ -x "$(command -v yarn)" ]; then
+        install_yarn
+    fi
+
+    run_tests
+}
+
+main "$@"
